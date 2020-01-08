c16f886fba9a03697b6a4ded5667a6a530bfed2f
Alvin Seville <MrFresnel@yandex.ru>
2020-01-08 07:34:14 +0300

    closes #6

--- NETMouse - .NET release/Utils/PABCSystem.cs
+++ NETMouse - .NET release/Utils/PABCSystem.cs
@@ -6,11 +6,13 @@ namespace ABCNET.Utils
     /// <summary>
     /// Добавлен ради совместимости с PABCSystem.
     /// </summary>
+    [Obsolete(ObsoleteMessageHelper.Message)]
     public static class PABCSystem
     {
         /// <summary>
         /// Возвращает словарь пар элементов (ключ, значение).
         /// </summary>
+        [Obsolete(ObsoleteMessageHelper.Message)]
         public static Dictionary<TKey, TValue> Dict<TKey, TValue>(params KeyValuePair<TKey, TValue>[] pairs)
         {
             if (pairs == null)
@@ -26,6 +28,7 @@ namespace ABCNET.Utils
         /// <summary>
         /// Возвращает словарь пар элементов (ключ, значение).
         /// </summary>
+        [Obsolete(ObsoleteMessageHelper.Message)]
         public static Dictionary<TKey, TValue> Dict<TKey, TValue>(params Tuple<TKey, TValue>[] pairs)
         {
             if (pairs == null)
@@ -41,6 +44,7 @@ namespace ABCNET.Utils
         /// <summary>
         ///  Возвращает множество на базе хеш таблицы, заполненное указанными значениями.
         /// </summary>
+        [Obsolete(ObsoleteMessageHelper.Message)]
         public static HashSet<T> HSet<T>(params T[] values)
         {
             if (values == null)
@@ -52,6 +56,7 @@ namespace ABCNET.Utils
         /// <summary>
         /// Возвращает множество на базе хеш таблицы, заполненное значениями из последовательности.
         /// </summary>
+        [Obsolete(ObsoleteMessageHelper.Message)]
         public static HashSet<T> HSet<T>(IEnumerable<T> values)
         {
             if (values == null)
@@ -63,6 +68,7 @@ namespace ABCNET.Utils
         /// <summary>
         /// Возвращает пару элементов (ключ, значение).
         /// </summary>
+        [Obsolete(ObsoleteMessageHelper.Message)]
         public static KeyValuePair<TKey, TValue> KV<TKey, TValue>(TKey key, TValue value)
         {
             return new KeyValuePair<TKey, TValue>(key, value);
@@ -71,6 +77,7 @@ namespace ABCNET.Utils
         /// <summary>
         /// Возвращает двусвязный список, заполненный указанными значениями.
         /// </summary>
+        [Obsolete(ObsoleteMessageHelper.Message)]
         public static LinkedList<T> LLst<T>(params T[] values)
         {
             if (values == null)
@@ -82,6 +89,7 @@ namespace ABCNET.Utils
         /// <summary>
         /// Возвращает двусвязный список, заполненный значениями из последовательности.
         /// </summary>
+        [Obsolete(ObsoleteMessageHelper.Message)]
         public static LinkedList<T> LLst<T>(IEnumerable<T> values)
         {
             if (values == null)
@@ -93,6 +101,7 @@ namespace ABCNET.Utils
         /// <summary>
         /// Возвращает список, заполненный указанными значениями.
         /// </summary>
+        [Obsolete(ObsoleteMessageHelper.Message)]
         public static List<T> Lst<T>(params T[] values)
         {
             if (values == null)
@@ -104,6 +113,7 @@ namespace ABCNET.Utils
         /// <summary>
         /// Возвращает список, заполненный значениями из последовательности.
         /// </summary>
+        [Obsolete(ObsoleteMessageHelper.Message)]
         public static List<T> Lst<T>(IEnumerable<T> values)
         {
             if (values == null)
@@ -115,6 +125,7 @@ namespace ABCNET.Utils
         /// <summary>
         /// Возвращает множество на базе бинарного дерева поиска, заполненное указанными значениями.
         /// </summary>
+        [Obsolete(ObsoleteMessageHelper.Message)]
         public static SortedSet<T> SSet<T>(params T[] values)
         {
             if (values == null)
@@ -126,6 +137,7 @@ namespace ABCNET.Utils
         /// <summary>
         /// <summary>Возвращает множество на базе бинарного дерева поиска, заполненное значениями из последовательности.
         /// </summary>
+        [Obsolete(ObsoleteMessageHelper.Message)]
         public static SortedSet<T> SSet<T>(IEnumerable<T> values)
         {
             if (values == null)
