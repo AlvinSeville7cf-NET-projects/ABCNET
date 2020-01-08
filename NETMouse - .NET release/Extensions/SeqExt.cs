60ce233214668dcbd78624c566bccf4a8eaf7937
Alvin Seville <MrFresnel@yandex.ru>
2020-01-08 16:01:02 +0300

    Extensions.SeqExt refactoring

--- NETMouse - .NET release/Extensions/SeqExt.cs
+++ NETMouse - .NET release/Extensions/SeqExt.cs
@@ -18,9 +18,9 @@ namespace ABCNET.Extensions
         public static IEnumerable<AssociateRes<T, TOutput>> Associate<T, TOutput>(this IEnumerable<T> collection, Func<T, TOutput> selector)
         {
             if (selector == null)
-                throw new ArgumentNullException("selector");
+                throw new ArgumentNullException(nameof(selector));
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
 
             return collection.Select(delegate (T x) { return new AssociateRes<T, TOutput>(x, selector(x)); });
         }
@@ -34,9 +34,9 @@ namespace ABCNET.Extensions
         public static IEnumerable<Tuple<T, T1>> ZipTuple<T, T1>(this IEnumerable<T> collection, IEnumerable<T1> secondCollection)
         {
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
             if (secondCollection == null)
-                throw new ArgumentNullException("secondCollection");
+                throw new ArgumentNullException(nameof(secondCollection));
 
             return collection.Zip(secondCollection, delegate (T x, T1 y) { return Tuple.Create(x, y); });
         }
@@ -49,7 +49,7 @@ namespace ABCNET.Extensions
         public static Tuple<IEnumerable<T>, IEnumerable<T1>> UnzipTuple<T, T1>(this IEnumerable<Tuple<T, T1>> collection)
         {
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
 
             return Tuple.Create(collection.Select(delegate (Tuple<T, T1> x) { return x.Item1; }),
                 collection.Select(delegate (Tuple<T, T1> x) { return x.Item2; }));
@@ -63,7 +63,7 @@ namespace ABCNET.Extensions
         public static IEnumerable<NumerateRes<T>> Numerate<T>(this IEnumerable<T> collection)
         {
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
 
             return collection.Select(delegate(T x, int i) { return new NumerateRes<T>(x, i); });
         }
@@ -77,9 +77,9 @@ namespace ABCNET.Extensions
         public static IEnumerable<NumerateRes<TOutput>> Numerate<T, TOutput>(this IEnumerable<T> collection, Func<T, TOutput> selector)
         {
             if (selector == null)
-                throw new ArgumentNullException("selector");
+                throw new ArgumentNullException(nameof(selector));
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
 
             return collection.Select(delegate(T x, int i) { return new NumerateRes<TOutput>(selector(x), i); });
         }
@@ -92,7 +92,7 @@ namespace ABCNET.Extensions
         public static IOrderedEnumerable<T> Sort<T>(this IEnumerable<T> collection)
         {
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
 
             return collection.OrderBy(x => x);
         }
@@ -105,7 +105,7 @@ namespace ABCNET.Extensions
         public static IOrderedEnumerable<T> SortDesc<T>(this IEnumerable<T> collection)
         {
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
             
             return collection.OrderByDescending(x => x);
         }
@@ -119,7 +119,7 @@ namespace ABCNET.Extensions
         public static T MinBy<T, TOutput>(this IEnumerable<T> collection, Func<T, TOutput> selector)
         {
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
             
             IComparer<TOutput> comparer = Comparer<TOutput>.Default;
             return collection.Aggregate((a, b) => comparer.Compare(selector(a), selector(b)) < 0 ? a : b);
@@ -134,7 +134,7 @@ namespace ABCNET.Extensions
         public static T MaxBy<T, TOutput>(this IEnumerable<T> collection, Func<T, TOutput> selector)
         {
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
             
             IComparer<TOutput> comparer = Comparer<TOutput>.Default;
             return collection.Aggregate((a, b) => comparer.Compare(selector(a), selector(b)) > 0 ? a : b);
@@ -149,9 +149,9 @@ namespace ABCNET.Extensions
         public static IEnumerable<Tuple<T, T1>> Cartesian<T, T1>(this IEnumerable<T> collectionFirst, IEnumerable<T1> collectionSecond)
         {
             if (collectionFirst == null)
-                throw new ArgumentNullException("collectionFirst");
+                throw new ArgumentNullException(nameof(collectionFirst));
             if (collectionSecond == null)
-                throw new ArgumentNullException("collectionSecond");
+                throw new ArgumentNullException(nameof(collectionSecond));
 
             foreach (T itemA in collectionFirst)
                 foreach (T1 itemB in collectionSecond)
@@ -168,11 +168,11 @@ namespace ABCNET.Extensions
         public static IEnumerable<Tuple<TOutput, TOutput>> Cartesian<T, TOutput>(this IEnumerable<T> collectionFirst, IEnumerable<T> collectionSecond, Func<T, TOutput> selector)
         {
             if (selector == null)
-                throw new ArgumentNullException("selector");
+                throw new ArgumentNullException(nameof(selector));
             if (collectionFirst == null)
-                throw new ArgumentNullException("collectionFirst");
+                throw new ArgumentNullException(nameof(collectionFirst));
             if (collectionSecond == null)
-                throw new ArgumentNullException("collectionSecond");
+                throw new ArgumentNullException(nameof(collectionSecond));
 
             foreach (T itemA in collectionFirst)
                 foreach (T itemB in collectionSecond)
@@ -187,7 +187,7 @@ namespace ABCNET.Extensions
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> collection)
         {
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
 
             Random random = new Random();
             return collection.OrderBy(x => random.Next(int.MaxValue));
@@ -202,9 +202,9 @@ namespace ABCNET.Extensions
         public static IEnumerable<TOutput> Shuffle<T, TOutput>(this IEnumerable<T> collection, Func<T, TOutput> selector)
         {
             if (selector == null)
-                throw new ArgumentNullException("selector");
+                throw new ArgumentNullException(nameof(selector));
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
 
             Random random = new Random();
             return collection.Select(x => selector(x)).OrderBy(x => random.Next(int.MaxValue));
@@ -219,7 +219,7 @@ namespace ABCNET.Extensions
         public static IEnumerable<T> Print<T>(this IEnumerable<T> collection, string delimiter = DefaultDelimiterHelper.Delimiter)
         {
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
             
             if (typeof(T) == typeof(char))
                 delimiter = string.Empty;
@@ -249,7 +249,7 @@ namespace ABCNET.Extensions
         public static IEnumerable<T> PrintLines<T>(this IEnumerable<T> collection)
         {
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
             
             foreach (T item in collection)
                 Console.WriteLine(item);
@@ -266,9 +266,9 @@ namespace ABCNET.Extensions
         public static IEnumerable<T> PrintBy<T, TOutput>(this IEnumerable<T> collection, Func<T, TOutput> selector, string delimiter = DefaultDelimiterHelper.Delimiter)
         {
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
             if (selector == null)
-                throw new ArgumentNullException("selector");
+                throw new ArgumentNullException(nameof(selector));
             
             if (typeof(T) == typeof(char))
                 delimiter = string.Empty;
@@ -300,9 +300,9 @@ namespace ABCNET.Extensions
         public static IEnumerable<T> PrintLinesBy<T, TOutput>(this IEnumerable<T> collection, Func<T, TOutput> selector)
         {
             if (collection == null)
-                throw new ArgumentNullException("collection");
+                throw new ArgumentNullException(nameof(collection));
             if (selector == null)
-                throw new ArgumentNullException("selector");
+                throw new ArgumentNullException(nameof(selector));
             
             foreach (T item in collection)
                 Console.WriteLine(selector(item));
