a59a8547d806d4cb8dce2ceff23e688c9e5f596a
Alvin Seville <MrFresnel@yandex.ru>
2020-01-08 04:41:52 +0300

    closes #4

---
+++ NETMouse - .NET release/Utils/PABCSystem.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ABCNET.Utils
+{
+    /// <summary>
+    /// Добавлен ради совместимости с PABCSystem.
+    /// </summary>
+    public static class PABCSystem
+    {
+        /// <summary>
+        /// Возвращает словарь пар элементов (ключ, значение).
+        /// </summary>
+        public static Dictionary<TKey, TValue> Dict<TKey, TValue>(params KeyValuePair<TKey, TValue>[] pairs)
+        {
+            if (pairs == null)
+                throw new ArgumentNullException("pairs");
+
+            Dictionary<TKey, TValue> d = new Dictionary<TKey, TValue>();
+            foreach (var item in pairs)
+                d.Add(item.Key, item.Value);
+
+            return d;
+        }
+
+        /// <summary>
+        /// Возвращает словарь пар элементов (ключ, значение).
+        /// </summary>
+        public static Dictionary<TKey, TValue> Dict<TKey, TValue>(params Tuple<TKey, TValue>[] pairs)
+        {
+            if (pairs == null)
+                throw new ArgumentNullException("pairs");
+
+            Dictionary<TKey, TValue> d = new Dictionary<TKey, TValue>();
+            foreach (var item in pairs)
+                d.Add(item.Item1, item.Item2);
+
+            return d;
+        }
+
+        /// <summary>
+        ///  Возвращает множество на базе хеш таблицы, заполненное указанными значениями.
+        /// </summary>
+        public static HashSet<T> HSet<T>(params T[] values)
+        {
+            if (values == null)
+                throw new ArgumentNullException("values");
+
+            return new HashSet<T>(values);
+        }
+
+        /// <summary>
+        /// Возвращает множество на базе хеш таблицы, заполненное значениями из последовательности.
+        /// </summary>
+        public static HashSet<T> HSet<T>(IEnumerable<T> values)
+        {
+            if (values == null)
+                throw new ArgumentNullException("values");
+
+            return new HashSet<T>(values);
+        }
+
+        /// <summary>
+        /// Возвращает пару элементов (ключ, значение).
+        /// </summary>
+        public static KeyValuePair<TKey, TValue> KV<TKey, TValue>(TKey key, TValue value)
+        {
+            return new KeyValuePair<TKey, TValue>(key, value);
+        }
+
+        /// <summary>
+        /// Возвращает двусвязный список, заполненный указанными значениями.
+        /// </summary>
+        public static LinkedList<T> LLst<T>(params T[] values)
+        {
+            if (values == null)
+                throw new ArgumentNullException("values");
+
+            return new LinkedList<T>(values);
+        }
+
+        /// <summary>
+        /// Возвращает двусвязный список, заполненный значениями из последовательности.
+        /// </summary>
+        public static LinkedList<T> LLst<T>(IEnumerable<T> values)
+        {
+            if (values == null)
+                throw new ArgumentNullException("values");
+
+            return new LinkedList<T>(values);
+        }
+
+        /// <summary>
+        /// Возвращает список, заполненный указанными значениями.
+        /// </summary>
+        public static List<T> Lst<T>(params T[] values)
+        {
+            if (values == null)
+                throw new ArgumentNullException("values");
+
+            return new List<T>(values);
+        }
+
+        /// <summary>
+        /// Возвращает список, заполненный значениями из последовательности.
+        /// </summary>
+        public static List<T> Lst<T>(IEnumerable<T> values)
+        {
+            if (values == null)
+                throw new ArgumentNullException("values");
+
+            return new List<T>(values);
+        }
+
+        /// <summary>
+        /// Возвращает множество на базе бинарного дерева поиска, заполненное указанными значениями.
+        /// </summary>
+        public static SortedSet<T> SSet<T>(params T[] values)
+        {
+            if (values == null)
+                throw new ArgumentNullException("values");
+
+            return new SortedSet<T>(values);
+        }
+
+        /// <summary>
+        /// <summary>Возвращает множество на базе бинарного дерева поиска, заполненное значениями из последовательности.
+        /// </summary>
+        public static SortedSet<T> SSet<T>(IEnumerable<T> values)
+        {
+            if (values == null)
+                throw new ArgumentNullException("values");
+
+            return new SortedSet<T>(values);
+        }
+    }
+}
