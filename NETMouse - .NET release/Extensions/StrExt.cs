bcfdc2ac41cbfce31449e82bf581c15368c32687
Михаил Завёрткин <misha.zavertkin@mail.ru>
2020-01-15 17:53:56 +0700

    closes #25

---
+++ NETMouse - .NET release/Extensions/StrExt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ABCNET.Extensions
+{
+    /// <summary>
+    /// Предоставляет функционал для работы со строками методами последовательностей.
+    /// </summary>
+    public static class StrExt
+    {
+        /// <summary>
+        /// Выводит строку на экран и делает переход на следующую строку.
+        /// </summary>
+        /// <param name="input">Строка.</param>
+        /// <returns>Строка.</returns>
+        public static string Println(this string input)
+        {
+            Console.WriteLine(input);
+            return input;
+        }
+        /// <summary>
+        /// Выводит строку на экран.
+        /// </summary>
+        /// <param name="input">Строка.</param>
+        /// <returns>Строка.</returns>
+        public static string Print(this string input)
+        {
+            Console.Write(input);
+            return input;
+        }
+    }
+}
