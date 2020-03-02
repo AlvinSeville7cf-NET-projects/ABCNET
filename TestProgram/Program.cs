479cb79fb2ebbbae0dd1990927bedeb20d389d8a
Alvin Seville <MrFresnel@yandex.ru>
2020-03-02 09:30:22 +0300

    critical bug fix 2

--- TestProgram/Program.cs
+++ TestProgram/Program.cs
@@ -24,7 +24,7 @@ namespace TestProgram
 
         private static void Main(string[] args)
         {
-            Matr.Rand(2, 3, 0, 10).PrintBy(x => x);
+            Matr.Rand(2, 3, 0, 10).PrintBy(x => x, ", ", "[", "]");
             "Hell".Println();
             //Matr.ReadInt32(Base.ReadInt32("N:"), Base.ReadInt32("M:"), "Элемент ({0}, {1})-ый:").Count().Numerate().MaxBy(x => x.Item).Index.Println();
         }
