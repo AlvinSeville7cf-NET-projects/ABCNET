e006c94644aa271326be9854c6226dc6d49d9090
Михаил Завёрткин <misha.zavertkin@mail.ru>
2020-03-06 19:11:51 +0700

    close #26

--- NETMouse - .NET release/Utils/Base.Input.cs
+++ NETMouse - .NET release/Utils/Base.Input.cs
@@ -9,6 +9,7 @@ namespace ABCNET.Utils
     public static partial class Base
     {
         #region public
+
         /// <summary>
         /// Читает значение типа Boolean. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
         /// </summary>
@@ -18,6 +19,105 @@ namespace ABCNET.Utils
         {
             prompt.Print();
             return bool.Parse(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Читает значение типа Byte. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        /// <returns>Значение.</returns>
+        public static byte ReadByte(string prompt = EmptyStringHelper.Empty)
+        {
+            prompt.Print();
+            return byte.Parse(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Читает значение типа Sbyte. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        /// <returns>Значение.</returns>
+        public static sbyte ReadSbyte(string prompt = EmptyStringHelper.Empty)
+        {
+            prompt.Print();
+            return sbyte.Parse(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Читает значение типа Decimal. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        /// <returns>Значение.</returns>
+        public static decimal ReadDecimal(string prompt = EmptyStringHelper.Empty)
+        {
+            prompt.Print();
+            return decimal.Parse(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Читает значение типа Float. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        /// <returns>Значение.</returns>
+        public static float ReadFloat(string prompt = EmptyStringHelper.Empty)
+        {
+            prompt.Print();
+            return float.Parse(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Читает значение типа Uint. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        /// <returns>Значение.</returns>
+        public static uint ReadUint(string prompt = EmptyStringHelper.Empty)
+        {
+            prompt.Print();
+            return uint.Parse(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Читает значение типа Long. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        /// <returns>Значение.</returns>
+        public static long ReadLong(string prompt = EmptyStringHelper.Empty)
+        {
+            prompt.Print();
+            return long.Parse(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Читает значение типа Ulong. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        /// <returns>Значение.</returns>
+        public static ulong ReadUlong(string prompt = EmptyStringHelper.Empty)
+        {
+            prompt.Print();
+            return ulong.Parse(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Читает значение типа Short. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        /// <returns>Значение.</returns>
+        public static short ReadShort(string prompt = EmptyStringHelper.Empty)
+        {
+            prompt.Print();
+            return short.Parse(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Читает значение типа Ushort. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        /// <returns>Значение.</returns>
+        public static ushort ReadUshort(string prompt = EmptyStringHelper.Empty)
+        {
+            prompt.Print();
+            return ushort.Parse(Console.ReadLine());
         }
 
         /// <summary>
@@ -74,7 +174,7 @@ namespace ABCNET.Utils
         }
 
         /// <summary>
-        /// Генерирует случайное число [low..high].
+        /// Генерирует случайное число [low;high].
         /// </summary>
         /// <param name="low">Нижняя граница диапазона.</param>
         /// <param name="high">Верхняя граница диапазона.</param>
@@ -88,7 +188,7 @@ namespace ABCNET.Utils
         }
 
         /// <summary>
-        /// Генерирует случайное число [low..high].
+        /// Генерирует случайное число [low;high].
         /// </summary>
         /// <param name="low">Нижняя граница диапазона.</param>
         /// <param name="high">Верхняя граница диапазона.</param>
@@ -100,6 +200,7 @@ namespace ABCNET.Utils
 
             return low + Rand() * (high - low);
         }
+
         #endregion public
     }
 }
---
+++ NETMouse - .NET release/Utils/Base.Nullable.Input.cs
@@ -0,0 +1,176 @@
+﻿using ABCNET.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ABCNET.Utils
+{
+    public static partial class Base
+    {
+
+        public static partial class Nullable
+        {
+
+            #region public
+
+            /// <summary>
+            /// Читает значение типа Boolean?. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+            /// </summary>
+            /// <param name="prompt">Приглашение к вводу.</param>
+            /// <returns>Значение.</returns>
+            public static bool? ReadBoolean(string prompt = EmptyStringHelper.Empty)
+            {
+                string result = InternalReadNull(prompt);
+                return result == null ? null : new bool?(bool.Parse(result));
+            }
+
+            /// <summary>
+            /// Читает значение типа Byte?. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+            /// </summary>
+            /// <param name="prompt">Приглашение к вводу.</param>
+            /// <returns>Значение.</returns>
+            public static byte? ReadByte(string prompt = EmptyStringHelper.Empty)
+            {
+                string result = InternalReadNull(prompt);
+                return result == null ? null : new byte?(byte.Parse(result));
+            }
+
+            /// <summary>
+            /// Читает значение типа Sbyte?. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+            /// </summary>
+            /// <param name="prompt">Приглашение к вводу.</param>
+            /// <returns>Значение.</returns>
+            public static sbyte? ReadSbyte(string prompt = EmptyStringHelper.Empty)
+            {
+                string result = InternalReadNull(prompt);
+                return result == null ? null : new sbyte?(sbyte.Parse(result));
+            }
+
+            /// <summary>
+            /// Читает значение типа Decimal?. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+            /// </summary>
+            /// <param name="prompt">Приглашение к вводу.</param>
+            /// <returns>Значение.</returns>
+            public static decimal? ReadDecimal(string prompt = EmptyStringHelper.Empty)
+            {
+                string result = InternalReadNull(prompt);
+                return result == null ? null : new decimal?(decimal.Parse(result));
+            }
+
+            /// <summary>
+            /// Читает значение типа Float?. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+            /// </summary>
+            /// <param name="prompt">Приглашение к вводу.</param>
+            /// <returns>Значение.</returns>
+            public static float? ReadFloat(string prompt = EmptyStringHelper.Empty)
+            {
+                string result = InternalReadNull(prompt);
+                return result == null ? null : new float?(float.Parse(result));
+            }
+
+            /// <summary>
+            /// Читает значение типа Uint?. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+            /// </summary>
+            /// <param name="prompt">Приглашение к вводу.</param>
+            /// <returns>Значение.</returns>
+            public static uint? ReadUint(string prompt = EmptyStringHelper.Empty)
+            {
+                string result = InternalReadNull(prompt);
+                return result == null ? null : new uint?(uint.Parse(result));
+            }
+
+            /// <summary>
+            /// Читает значение типа Long?. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+            /// </summary>
+            /// <param name="prompt">Приглашение к вводу.</param>
+            /// <returns>Значение.</returns>
+            public static long? ReadLong(string prompt = EmptyStringHelper.Empty)
+            {
+                string result = InternalReadNull(prompt);
+                return result == null ? null : new long?(long.Parse(result));
+            }
+
+            /// <summary>
+            /// Читает значение типа Ulong?. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+            /// </summary>
+            /// <param name="prompt">Приглашение к вводу.</param>
+            /// <returns>Значение.</returns>
+            public static ulong? ReadUlong(string prompt = EmptyStringHelper.Empty)
+            {
+                string result = InternalReadNull(prompt);
+                return result == null ? null : new ulong?(ulong.Parse(result));
+            }
+
+            /// <summary>
+            /// Читает значение типа Short?. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+            /// </summary>
+            /// <param name="prompt">Приглашение к вводу.</param>
+            /// <returns>Значение.</returns>
+            public static short? ReadShort(string prompt = EmptyStringHelper.Empty)
+            {
+                string result = InternalReadNull(prompt);
+                return result == null ? null : new short?(short.Parse(result));
+            }
+
+            /// <summary>
+            /// Читает значение типа Ushort?. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+            /// </summary>
+            /// <param name="prompt">Приглашение к вводу.</param>
+            /// <returns>Значение.</returns>
+            public static ushort? ReadUshort(string prompt = EmptyStringHelper.Empty)
+            {
+                string result = InternalReadNull(prompt);
+                return result == null ? null : new ushort?(ushort.Parse(result));
+            }
+
+            /// <summary>
+            /// Читает значение типа Char?. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+            /// </summary>
+            /// <param name="prompt">Приглашение к вводу.</param>
+            /// <returns>Значение.</returns>
+            public static char? ReadChar(string prompt = EmptyStringHelper.Empty)
+            {
+                string result = InternalReadNull(prompt);
+                return result == null ? null : new char?(char.Parse(result));
+            }
+
+            /// <summary>
+            /// Читает значение типа Double?. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+            /// </summary>
+            /// <param name="prompt">Приглашение к вводу.</param>
+            /// <returns>Значение.</returns>
+            public static double? ReadDouble(string prompt = EmptyStringHelper.Empty)
+            {
+                string result = InternalReadNull(prompt);
+                return result == null ? null : new double?(double.Parse(result));
+            }
+
+            /// <summary>
+            /// Читает значение типа Int32?. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+            /// </summary>
+            /// <param name="prompt">Приглашение к вводу.</param>
+            /// <returns>Значение.</returns>
+            public static int? ReadInt32(string prompt = EmptyStringHelper.Empty)
+            {
+                string result = InternalReadNull(prompt);
+                return result == null ? null : new int?(int.Parse(result));
+            }
+
+            #endregion
+
+            #region internal
+
+            internal static string InternalReadNull(string prompt = EmptyStringHelper.Empty)
+            {
+                prompt.Print();
+                string result = Console.ReadLine().Trim(new char[] { ' ' });
+                return result.Trim(' ') == "null" ? null : result;
+            }
+
+            #endregion
+
+        }
+
+    }
+}
