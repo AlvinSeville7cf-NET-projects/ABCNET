f0af979e223b15e19979d324d80ff01fd1b39b87
Михаил Завёрткин <misha.zavertkin@mail.ru>
2020-04-04 13:09:37 +0700

    closes #28 Extensions

--- NETMouse - .NET release/Extensions/MatrixE.Input.cs
+++ NETMouse - .NET release/Extensions/MatrixE.Input.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 using ABCNET.Utils;
 
 namespace ABCNET.Extensions
@@ -9,6 +10,7 @@ namespace ABCNET.Extensions
     public static partial class MatrixE
     {
         #region public
+
         /// <summary>
         /// Заполняет матрицу значениями типа Boolean. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
         /// </summary>
@@ -41,6 +43,68 @@ namespace ABCNET.Extensions
         }
 
         /// <summary>
+        /// Заполняет матрицу значениями типа Byte. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="matrix">Матрица.</param>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        public static void Read(this byte[,] matrix, string prompt = EmptyStringHelper.Empty)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+
+            int i = 0;
+            int j = 0;
+
+            while (i < matrix.GetLength(0))
+            {
+                while (j < matrix.GetLength(1))
+                    try
+                    {
+                        matrix[i, j] = Base.ReadByte(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+                        j++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(InputErrorHelper.Message);
+                    }
+
+                i++;
+                j = 0;
+            }
+        }
+
+        /// <summary>
+        /// Заполняет матрицу значениями типа SByte. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="matrix">Матрица.</param>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        public static void Read(this sbyte[,] matrix, string prompt = EmptyStringHelper.Empty)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+
+            int i = 0;
+            int j = 0;
+
+            while (i < matrix.GetLength(0))
+            {
+                while (j < matrix.GetLength(1))
+                    try
+                    {
+                        matrix[i, j] = Base.ReadSByte(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+                        j++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(InputErrorHelper.Message);
+                    }
+
+                i++;
+                j = 0;
+            }
+        }
+
+        /// <summary>
         /// Заполняет матрицу значениями типа Char. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
         /// </summary>
         /// <param name="matrix">Матрица.</param>
@@ -50,9 +114,56 @@ namespace ABCNET.Extensions
             if (matrix == null)
                 throw new ArgumentNullException(nameof(matrix));
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                    matrix[i, j] = Base.ReadChar(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+            int i = 0;
+            int j = 0;
+
+            while (i < matrix.GetLength(0))
+            {
+                while (j < matrix.GetLength(1))
+                    try
+                    {
+                        matrix[i, j] = Base.ReadChar(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+                        j++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(InputErrorHelper.Message);
+                    }
+
+                i++;
+                j = 0;
+            }
+        }
+
+        /// <summary>
+        /// Заполняет матрицу значениями типа Decimal. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="matrix">Матрица.</param>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        public static void Read(this decimal[,] matrix, string prompt = EmptyStringHelper.Empty)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+
+            int i = 0;
+            int j = 0;
+
+            while (i < matrix.GetLength(0))
+            {
+                while (j < matrix.GetLength(1))
+                    try
+                    {
+                        matrix[i, j] = Base.ReadDecimal(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+                        j++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(InputErrorHelper.Message);
+                    }
+
+                i++;
+                j = 0;
+            }
         }
 
         /// <summary>
@@ -74,6 +185,37 @@ namespace ABCNET.Extensions
                     try
                     {
                         matrix[i, j] = Base.ReadDouble(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+                        j++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(InputErrorHelper.Message);
+                    }
+
+                i++;
+                j = 0;
+            }
+        }
+
+        /// <summary>
+        /// Заполняет матрицу значениями типа Single. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="matrix">Матрица.</param>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        public static void Read(this float[,] matrix, string prompt = EmptyStringHelper.Empty)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+
+            int i = 0;
+            int j = 0;
+
+            while (i < matrix.GetLength(0))
+            {
+                while (j < matrix.GetLength(1))
+                    try
+                    {
+                        matrix[i, j] = Base.ReadSingle(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
                         j++;
                     }
                     catch (Exception)
@@ -118,6 +260,192 @@ namespace ABCNET.Extensions
         }
 
         /// <summary>
+        /// Заполняет матрицу значениями типа Uint32. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="matrix">Матрица.</param>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        public static void Read(this uint[,] matrix, string prompt = EmptyStringHelper.Empty)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+
+            int i = 0;
+            int j = 0;
+
+            while (i < matrix.GetLength(0))
+            {
+                while (j < matrix.GetLength(1))
+                    try
+                    {
+                        matrix[i, j] = Base.ReadUint32(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+                        j++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(InputErrorHelper.Message);
+                    }
+
+                i++;
+                j = 0;
+            }
+        }
+
+        /// <summary>
+        /// Заполняет матрицу значениями типа Int64. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="matrix">Матрица.</param>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        public static void Read(this long[,] matrix, string prompt = EmptyStringHelper.Empty)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+
+            int i = 0;
+            int j = 0;
+
+            while (i < matrix.GetLength(0))
+            {
+                while (j < matrix.GetLength(1))
+                    try
+                    {
+                        matrix[i, j] = Base.ReadInt64(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+                        j++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(InputErrorHelper.Message);
+                    }
+
+                i++;
+                j = 0;
+            }
+        }
+
+        /// <summary>
+        /// Заполняет матрицу значениями типа UInt64. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="matrix">Матрица.</param>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        public static void Read(this ulong[,] matrix, string prompt = EmptyStringHelper.Empty)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+
+            int i = 0;
+            int j = 0;
+
+            while (i < matrix.GetLength(0))
+            {
+                while (j < matrix.GetLength(1))
+                    try
+                    {
+                        matrix[i, j] = Base.ReadUInt64(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+                        j++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(InputErrorHelper.Message);
+                    }
+
+                i++;
+                j = 0;
+            }
+        }
+
+        /// <summary>
+        /// Заполняет матрицу значениями типа Int16. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="matrix">Матрица.</param>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        public static void Read(this short[,] matrix, string prompt = EmptyStringHelper.Empty)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+
+            int i = 0;
+            int j = 0;
+
+            while (i < matrix.GetLength(0))
+            {
+                while (j < matrix.GetLength(1))
+                    try
+                    {
+                        matrix[i, j] = Base.ReadInt16(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+                        j++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(InputErrorHelper.Message);
+                    }
+
+                i++;
+                j = 0;
+            }
+        }
+
+        /// <summary>
+        /// Заполняет матрицу значениями типа UInt16. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="matrix">Матрица.</param>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        public static void Read(this ushort[,] matrix, string prompt = EmptyStringHelper.Empty)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+
+            int i = 0;
+            int j = 0;
+
+            while (i < matrix.GetLength(0))
+            {
+                while (j < matrix.GetLength(1))
+                    try
+                    {
+                        matrix[i, j] = Base.ReadUInt16(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+                        j++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(InputErrorHelper.Message);
+                    }
+
+                i++;
+                j = 0;
+            }
+        }
+
+        /// <summary>
+        /// Заполняет матрицу значениями типа BigInteger. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
+        /// </summary>
+        /// <param name="matrix">Матрица.</param>
+        /// <param name="prompt">Приглашение к вводу.</param>
+        public static void Read(this BigInteger[,] matrix, string prompt = EmptyStringHelper.Empty)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+
+            int i = 0;
+            int j = 0;
+
+            while (i < matrix.GetLength(0))
+            {
+                while (j < matrix.GetLength(1))
+                    try
+                    {
+                        matrix[i, j] = Base.ReadBigInteger(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+                        j++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(InputErrorHelper.Message);
+                    }
+
+                i++;
+                j = 0;
+            }
+        }
+
+        /// <summary>
         /// Заполняет матрицу значениями типа String. [Не работает при запуске из под оболочки в IDE PascalABC.NET.]
         /// </summary>
         /// <param name="matrix">Матрица.</param>
@@ -127,10 +455,27 @@ namespace ABCNET.Extensions
             if (matrix == null)
                 throw new ArgumentNullException(nameof(matrix));
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                    matrix[i, j] = Base.ReadString(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+            int i = 0;
+            int j = 0;
+
+            while (i < matrix.GetLength(0))
+            {
+                while (j < matrix.GetLength(1))
+                    try
+                    {
+                        matrix[i, j] = Base.ReadString(prompt is null ? EmptyStringHelper.Empty : string.Format(prompt, i, j));
+                        j++;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(InputErrorHelper.Message);
+                    }
+
+                i++;
+                j = 0;
+            }
         }
+
         #endregion public
     }
 }
