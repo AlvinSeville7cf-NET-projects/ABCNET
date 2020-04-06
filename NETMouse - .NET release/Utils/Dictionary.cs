94c1fb0934868989c10f7e238cd22f70543bc2ac
Alvin Seville <MrFresnel@yandex.ru>
2020-04-06 15:17:19 +0300

    closes #113 - Dictionary.cs

---
+++ NETMouse - .NET release/Utils/Dictionary.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ABCNET.Utils
+{
+    /// <summary>
+    /// Предоставляет функционал для работы со словарями.
+    /// </summary>
+    public static class Dictionary
+    {
+        /// <summary>
+        /// Создаёт словарь из указанных значений.
+        /// </summary>
+        /// <param name="values">Значения.</param>
+        /// <returns>Словарь.</returns>
+        public static Dictionary<TKey, TValue> Of<TKey, TValue>(params KeyValuePair<TKey, TValue>[] values)
+        {
+            if (values is null)
+                throw new ArgumentNullException(nameof(values));
+
+            Dictionary<TKey, TValue> result = new Dictionary<TKey, TValue>();
+            foreach (var item in values)
+                result.Add(item.Key, item.Value);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Создаёт словарь на основе функции селектора.
+        /// </summary>
+        /// <param name="count">Количество элементов.</param>
+        /// <param name="selector">Функция селектор.</param>
+        /// <param name="firstIndex">Начальный индекс.</param>
+        /// <returns>Словарь.</returns>
+        public static Dictionary<TKey, TValue> By<TKey, TValue>(int count, Func<int, KeyValuePair<TKey, TValue>> selector, int firstIndex = 0)
+        {
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector));
+
+            Dictionary<TKey, TValue> result = new Dictionary<TKey, TValue>();
+            for (int i = 0; i < count; i++)
+            {
+                KeyValuePair<TKey, TValue> keyValuePair = selector(i + firstIndex);
+                result.Add(keyValuePair.Key, keyValuePair.Value);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Создаёт словарь на основе функции селектора.
+        /// </summary>
+        /// <param name="count">Количество элементов.</param>
+        /// <param name="first">Первый элемент.</param>
+        /// <param name="next">Функция получения следующего элемента.</param>
+        /// <returns>Словарь.</returns>
+        public static Dictionary<TKey, TValue> By<TKey, TValue>(int count, KeyValuePair<TKey, TValue> first, Func<KeyValuePair<TKey, TValue>, KeyValuePair<TKey, TValue>> next)
+        {
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+            if (next is null)
+                throw new ArgumentNullException(nameof(next));
+
+            Dictionary<TKey, TValue> result = new Dictionary<TKey, TValue>
+            {
+                { first.Key, first.Value }
+            };
+
+            KeyValuePair<TKey, TValue> previous = first;
+            for (int i = 1; i < count; i++)
+            {
+                KeyValuePair<TKey, TValue> keyValuePair = next(previous);
+                result.Add(keyValuePair.Key, keyValuePair.Value);
+                previous = keyValuePair;
+            }
+
+            return result;
+        }
+    }
+}
