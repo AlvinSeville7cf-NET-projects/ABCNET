50c7fcf399d226c896d5b8b8a0c3ae12b417d153
Михаил Завёрткин <misha.zavertkin@mail.ru>
2020-06-10 15:20:22 +0700

    Little optimization and adding IsBetween() overload

--- NETMouse - .NET release/Extensions/BaseE.Other.cs
+++ NETMouse - .NET release/Extensions/BaseE.Other.cs
@@ -18,8 +18,22 @@
         public static bool IsBetween(this int target, int firstBorder, int secondBorder)
         {
             if (firstBorder > secondBorder)
-                Utils.BaseU.Swap(ref firstBorder, ref secondBorder);
+                return (target >= secondBorder) && (target <= firstBorder);
             return (target >= firstBorder) && (target <= secondBorder);
+        }
+
+        /// <summary>
+        /// Проверяет лежит ли число между другими двумя.
+        /// </summary>
+        /// <param name="target">Число.</param>
+        /// <param name="firstBorder">Первая граница.</param>
+        /// <param name="secondBorder">Вторая граница.</param>
+        /// <returns>Результат.</returns>
+        public static bool IsBetween(this int target, int firstBorder, bool equalsFirst, int secondBorder, bool equalsSecond)
+        {
+            if (firstBorder > secondBorder)
+                return (target > secondBorder || (equalsSecond && target == secondBorder)) && (target < firstBorder || (equalsFirst && target == firstBorder));
+            return (target > firstBorder || (equalsFirst && target == firstBorder)) && (target < secondBorder || (equalsSecond && target == secondBorder));
         }
 
         #endregion public
